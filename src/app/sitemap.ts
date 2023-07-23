56f89439a4922da993188c9103ae1de785820694
Umar <umarg1997@gmail.com>
2023-07-24 00:32:03 +0100

    fix: nav not closing when link tapped on mobile + added sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,33 @@
+import { getDrives } from "@/groq/getDrives";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const drives =
+    (await getDrives({
+      limit: 1000,
+    })) || [];
+  return [
+    {
+      url: "https://alldrives.xyz",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://alldrives.xyz/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://alldrives.xyz/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://alldrives.xyz/roadmap",
+      lastModified: new Date(),
+    },
+    ...drives.map((drive) => {
+      return {
+        url: `https://alldrives.xyz/drives/${drive.slug}`,
+        lastModified: new Date(drive._updatedAt),
+      };
+    }),
+  ];
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -87,6 +87,7 @@ export const Header = () => {
                   <Link
                     key={item.name}
                     href={item.href}
+                    onClick={() => setMobileMenuOpen(false)}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
                   >
                     {item.name}
